adf96ab52730d1af07527322ad34e4e98b6f2ca1
Jacob <hellgrenjacob@hotmail.com>
2025-03-31 15:33:30 +0200

    started on connection

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(express.bodyParser())
+
+mongoose.connect(process.env.MONG_URI);
