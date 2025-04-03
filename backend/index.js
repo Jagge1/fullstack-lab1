b77ab8e81b804248dd2940d9dc2fe3d0d3dd7881
Jacob <hellgrenjacob@hotmail.com>
2025-04-03 13:18:02 +0200

    Logic to get functions (get by name and get all) added

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
-const express = require('express');
 require('dotenv').config();
+const express = require('express');
 const mongoose = require('mongoose');
 const Dish = require('./dish.model.js');
 const app = express();
@@ -12,11 +12,36 @@ mongoose.connect(process.env.MONG_URI);
 
 app.get('/api/dishes', async (req, res) => {
 
+  try {
+
+    const dishes = await Dish.find({});
+
+    res.status(200).json(dishes);
+    console.log('Debug')
+    
+  } catch (error) {
+    res.status(500).json({message: error.message})
+  }
 });
 
 app.get('/api/dishes/:name', async (req, res) => {
 
-  const name = req.params;
+  const {name} = req.params;
+
+  try {
+    //Array of dishes of searched name
+    const dish = await Dish.find({name: name}); 
+
+    //Checks if array is empty
+    if (!dish.length) {
+       return res.status(404).json({message: 'Error: Dish not found'})   
+    } else {
+      res.status(200).json(dish);
+    }
+
+  } catch (error) {
+    res.status(500).json({message: error.message})
+  }
 
 });
 
@@ -26,13 +51,13 @@ app.post('/api/dishes', async (req, res) => {
 
 app.put('/api/dishes/:id', async (req, res) => {
 
-  const dishId = req.params;
+  const {dishId} = req.params;
 
 });
 
 app.delete('/api/dishes/:id', async (req, res) => {
 
-  const dishId = req.params;
+  const {dishId} = req.params;
 
 });
 
