197b8ee3256f2ce82c681f6b2a7c0180955e9e23
Jagge1 <hellgrenjacob@hotmail.com>
2025-04-07 13:04:25 +0200

    buttons added - no logic

--- frontend/script.js
+++ frontend/script.js
@@ -8,20 +8,24 @@ async function getDishes() {
     method: 'GET'
   });
   
-  const data = await response.json()
+  const data = await response.json();
 
   let table = document.getElementById('dish-table');
 
   for (let i = 0; i < data.length; i++) {
     let row = `
-      <tr>
-        <td>${data[i].name}</td>
-        <td>${(data[i].ingredients).join('')  }</td>
-        <td>${(data[i].cookingSteps).join('')  }</td>
-        <td>${data[i].cookingTime}</td>
-        <td>${data[i].flavorProfile}</td> 
-        <td>${data[i].origin}</td>      
-      </tr>
+      
+          <tr>
+            <td>${data[i].name}</td>
+            <td>${(data[i].ingredients).join('')  }</td>
+            <td>${(data[i].cookingSteps).join('')  }</td>
+            <td>${data[i].cookingTime}</td>
+            <td>${data[i].flavorProfile.join(' ') }</td> 
+            <td>${data[i].origin}</td>      
+          </tr> 
+          <div class="button-container">
+            <button id="update-btn">Update</button>  <button id="delete-btn">Delete</button>
+            </div>
     
     `;
     table.innerHTML += row;
@@ -30,3 +34,16 @@ async function getDishes() {
 }
 
 getDishes()
+
+async function deleteDish() {
+
+  const response = await fetch('/api/dishes/:name', {
+
+    headers: {
+      'Accept': 'application/json'
+    },
+    method: 'DELETE'
+  });
+
+  const data = await response.json();
+}
