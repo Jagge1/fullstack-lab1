fca5e92f5ad238b6c0b9a63b27de9bedb191a3d9
Jacob <hellgrenjacob@hotmail.com>
2025-04-07 23:14:39 +0200

    delete button works, update is yet to be tested

--- frontend/script.js
+++ frontend/script.js
@@ -22,29 +22,65 @@ async function getDishes() {
             <td>${data[i].cookingTime}</td>
             <td>${data[i].flavorProfile.join(' ') }</td> 
             <td>${data[i].origin}</td>      
-          <div class="button-container">
-            <button class="modal-btn update-btn" data-name="${data[i].name}">Update</button>  <button class="modal-btn delete-btn" data-name="${data[i].name}">Delete</button>
-            </div>
-
           </tr> 
+          <div class="button-container">
+            <button class="modal-btn update-btn" data-id="${data[i]._id}">Update</button>
+            <button class="modal-btn delete-btn" data-id="${data[i]._id}">Delete</button>
+            </div>
     `;
     table.innerHTML += row;
   }
 }
 
-getDishes()
+async function deleteDish(id) {
 
-async function deleteDish() {
-
-  const response = await fetch('/api/dishes/:name', {
+  const response = await fetch(`/api/dishes/${encodeURIComponent(id)}`, {
 
     headers: {
       'Accept': 'application/json'
     },
-    method: 'DELETE'
+    method: 'DELETE',
+
   });
 
-  const data = await response.json();
+  if (response.ok) {
+    console.log('Dish was deleted')
+  }
+  
+}
+
+function updateDish() {
+
+  document.getElementById('update-form').addEventListener('submit', async (e)=> {
+    e.preventDefault();
+
+    let name = document.getElementById('name-input').value;
+    let ingredients = document.getElementById('ingredients-input').value.split(',');
+    let cookingSteps = document.getElementById('cooking-steps-input').value.split(',');
+    let cookingTime = document.getElementById('cooking-time-input').value;
+    let flavorProfile = document.getElementById('flavor-profile-input').value.split(',');
+    let origin = document.getElementById('origin-input').value;
+
+    const updatedDish = {
+      name: name,
+      ingredients: ingredients,
+      cookingSteps: cookingSteps,
+      cookingTime: cookingTime,
+      flavorProfile: flavorProfile,
+      origin: origin
+    };
+
+    const response = await fetch(`/api/dishes/${encodeURIComponent(name)}`, {
+
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      method: 'PUT',
+      body: JSON.stringify(updatedDish)
+  
+    });
+});
 }
 
 function showModal(){
@@ -54,12 +90,17 @@ function showModal(){
   let yesButton = document.getElementById('yes-btn');
   let noButton = document.getElementById('no-btn');
   let description = document.getElementById('modal-description');
+  let modal = document.getElementById('myModal');
+  let dishToDelete = '';
+
   
   document.addEventListener('click', function(e) {
     if (e.target.classList.contains('update-btn')) {
       document.getElementById('myModal').style.display = 'block';
       title.innerText = 'Update Dish' 
       form.style.display = 'block'
+      yesButton.style.display = 'none'
+      noButton.style.display = 'none'   
     }
   });
 
@@ -70,12 +111,25 @@ function showModal(){
       description.innerText = 'Are you sure want to delete this dish?' 
       form.style.display = 'none'
       yesButton.style.display = 'inline-block'
-      noButton.style.display = 'inline-block'   
+      noButton.style.display = 'inline-block' 
+      dishToDelete = e.target.getAttribute('data-id');  
     }
   });
+
+  yesButton.addEventListener('click', ()=> {
+    console.log('Yes click')
+    deleteDish(dishToDelete);
+    modal.style.display = 'none';
+    window.location.reload()
+  })
+
   
   document.querySelector('.close').addEventListener('click', function() {
-    document.getElementById('myModal').style.display = 'none';
+    modal.style.display = 'none';
+  });
+
+  noButton.addEventListener('click', function() {
+    modal.style.display = 'none';
   });
   
   window.addEventListener('click', function(event) {
@@ -86,7 +140,7 @@ function showModal(){
   });
 }
 
+
+getDishes();
+updateDish();
 showModal();
-
-
-
